47a4bd06187e8982268bd6bd1ffa364e44e56051
maruf12 <maruf.harsono@gmail.com>
2023-01-03 10:09:17 +0700

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2 class="m-80">About</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero ut assumenda possimus corrupti. Qui architecto ipsam nulla enim at culpa nobis, sequi nam.</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
