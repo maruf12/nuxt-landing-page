b51c977c6f40f3e90e77701ed0569992b9e013e6
maruf12 <maruf.harsono@gmail.com>
2023-01-11 00:32:28 +0700

    Improve responsive section categories

--- components/Categories.vue
+++ components/Categories.vue
@@ -4,17 +4,42 @@
       <h2 class="font-semibold text-4xl leading-none tracking-wider lg:text-5xl lg:leading-none lg:tracking-wide text-[#2D3134] font-sans">Categories</h2>
       <p class="pt-4 font-extralight tracking-wide text-sm lg:text-base">Here are lots of interesting destinations to visit, but don't be confused—they're already grouped by category.</p>
     </div>
-    <div class="flex flex-col flex-auto lg:flex-row mt-10 justify-between">
-      <!-- Looping disini -->
-      <div v-for="item in 6" :key="item">
-        <img src="@/assets/img/categories-1.svg" alt="Beach" />
-        <p class="text-center font-medium text-xl mt-2 tracking-wider">{{ 'Beach' }}</p>
+    <div class="flex flex-wrap lg:flex lg:flex-row mt-10 justify-between">
+      <div v-for="item in categoriesData" :key="item" class="mx-5 basis-1/3 w-1/2 lg:basis-[13%] lg:w-full py-2">
+        <nuxt-img :src="item.img" loading="lazy" />
+        <p class="text-center font-medium text-xl mt-2 tracking-wider">{{ item.name }}</p>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
+  const categoriesData = ref([
+    {
+      name: 'Beach',
+      img: '/categories-1.png'
+    },
+    {
+      name: 'Desert',
+      img: '/categories-2.png'
+    },
+    {
+      name: 'Mountain',
+      img: '/categories-3.png'
+    },
+    {
+      name: 'Temple',
+      img: '/categories-4.png'
+    },
+    {
+      name: 'Tower',
+      img: '/categories-5.png'
+    },
+    {
+      name: 'Pyramid',
+      img: '/categories-6.png'
+    },
+  ])
 
 </script>
 
