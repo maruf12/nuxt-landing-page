a6d64076186aa917594957b3f6bbd250fa4ab705
maruf12 <maruf.harsono@gmail.com>
2023-01-11 01:05:43 +0700

    fixing nuxt image in prod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  image: {
+    dir: 'static/'
+  },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge',]
 })
